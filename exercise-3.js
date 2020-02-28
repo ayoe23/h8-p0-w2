9379a6ac0cb49c9e349ef1a2717a69b2f8083f05
michaelm96 <michaelarceus96@gmail.com>
2020-02-28 14:52:22 +0700

    exercise-3

---
+++ exercise-3.js
@@ -0,0 +1,25 @@
+//Input Nama
+var nama = 'Ayu';
+var peran = 'Tabib';
+
+if (nama === '') {
+    console.log('Nama harus di isi...');
+}
+else if (peran === '') {
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+}    
+
+
+if (peran === 'Ksatria')  {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+}
+else if (peran === 'Tabib' ) {
+    console.log('Selamat datang di Dunia Proxytia' + ',' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
+}
+else if (peran === 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxytia' + ',' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+}
+
